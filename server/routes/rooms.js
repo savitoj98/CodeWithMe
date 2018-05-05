d3e9f9111a6daa0bb3146e134b172d5ec9695cea
Savitoj <savitojsingh15@gmail.com>
2018-05-05 18:34:15 +0530

    compilation feature

--- server/routes/rooms.js
+++ server/routes/rooms.js
@@ -5,13 +5,11 @@ router.get('/', function(req, res, next) {
 
     var room_id = req.baseUrl.toString().split('').reverse().join('').substr(0,7);
 
-
-
-
     var context = {
         room_id: room_id
     };
     res.render('rooms', context);
 });
+
 
 module.exports = router;
