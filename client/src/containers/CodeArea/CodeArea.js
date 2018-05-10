f533e686120bb8eb10943dd0820e5d0eece5dc86
meghansh36 <meghansh.work@gmail.com>
2018-05-10 18:15:50 +0530

    bug fixes

--- client/src/containers/CodeArea/CodeArea.js
+++ client/src/containers/CodeArea/CodeArea.js
@@ -2,14 +2,16 @@ import React, {Component} from "react"
 import {Controlled as CodeMirror} from 'react-codemirror2'
 import ModeSelector from "../../components/CodeAreaComponents/ModeSelector"
 import CodeOutput from "./CodeOutput"
+import FileUpload from "../../components/CodeAreaComponents/FileUpload"
 // import Aux from "../../hoc/aux"
 import socketIOClient from "socket.io-client"
-
 import "codemirror/lib/codemirror.css"
 import "codemirror/theme/ambiance.css"
 import "codemirror/mode/javascript/javascript"
 import "codemirror/mode/clike/clike"
 import "codemirror/mode/python/python"
+import "codemirror/mode/ruby/ruby"
+import classes from "./CodeArea.css"
 
 class CodeArea extends Component{
     //INITIAL STATE
@@ -18,12 +20,14 @@ class CodeArea extends Component{
         mode: 'text/x-csrc',
         language: "C",
         theme: 'ambiance',
-        socketEndpoint: 'http://localhost:5000',
         roomId: this.props.roomId,
-        editing:false
+        editing:false,
+
     }
 
-    socket = socketIOClient(this.state.socketEndpoint) //CONNECTS SOCKET 
+    socketEndpoint='http://localhost:5000'
+
+    socket = socketIOClient(this.socketEndpoint) //CONNECTS SOCKET 
 
     beforeCodeUpdateHandler = (editor,data,value) => {  //BEFORE CHANGE HANDLER FOR CODEMIRROR
         this.setState({code: value})
@@ -36,8 +40,9 @@ class CodeArea extends Component{
     }
 
     codeUpdateHandler = (editor,data,newCode) => { //ONCHANGE HANDLER FOR CODEMIRROR. 
+        console.log(this.state.editing)
         if(this.state.editing){ //SOLVES TYPE GLITCH
-            // console.log('socket emitted')
+            //console.log("socket emitted")
             //emit socket for code update
             this.socket.emit("code_updated", {code: this.state.code, roomId: this.state.roomId}) 
         }
@@ -47,29 +52,51 @@ class CodeArea extends Component{
         this.setState({
             mode: mode,
             language: lang,
-            code: defaultCode
+            code: defaultCode.length > this.state.code.length ? defaultCode : this.state.code
         })
+    }
+
+    fileUploadHandler = (event) => {
+        let file = event.target.files[0];
+        console.log(file);
+        
+        if (file) {
+          let data = new FileReader();
+          data.onload = (e)=>{
+              this.setState({
+                  code: e.target.result,
+                  editing:true
+              })
+          }
+          data.readAsText(file)
+         // data.append('file', file);
+          // axios.post('/files', data)..
+          
+        }
     }
 
     //LIFECYCLE METHODS AND THEIR HELPER FUNCTIONS ARE DEFINED BELOW
 
     componentWillMount(){
         this.socket.emit("create_room", this.state.roomId)
+        //console.log(this.state.code)
     }
 
     componentDidMount(){  // HOOK FOR LIFECYCLE. LISTENS FOR INCOMING SOCKETS
-        this.socket.on("process code", (response) => {
-            this.updateCodeFromSockets(response.data.code)
-            // console.log("socket received")
-            
-        })
+            this.socket.on("process code", (response) => {
+                this.updateCodeFromSockets(response.data.code)
+                //console.log("socket received")  
+            })
+        
     }
-    updateCodeFromSockets = (payload) => {  //SETS NEW STATE. CHANGES EDITING TO FALSE. IMPORTANT! 
-                                            //SOLVES TYPE GLITCH
-        this.setState({
-            code: payload,
-            editing: false
-        })
+    updateCodeFromSockets = (payload) => {  
+        //SETS NEW STATE. CHANGES EDITING TO FALSE. IMPORTANT! 
+        //SOLVES TYPE GLITCH
+             
+            this.setState({
+                code: payload,
+                editing: false
+            })
         
     }
 
@@ -82,13 +109,13 @@ class CodeArea extends Component{
         // console.log(this.state.code)
         return (
         <div>
-            <div>
-                <CodeMirror value = {this.state.code} options = {options} onChange = {this.codeUpdateHandler} onBeforeChange = {this.beforeCodeUpdateHandler} onKeyPress={this.keyPressHandler} onKeyDown={this.keyPressHandler}/>
+            <div className={classes.CodeArea}>
+
+                <CodeMirror value = {this.state.code} options = {options} onChange = {this.codeUpdateHandler} onBeforeChange = {this.beforeCodeUpdateHandler} onKeyPress={this.keyPressHandler} onKeyDown={this.keyPressHandler} className = {classes.CodeEditor}/>
+
             </div>
-            
-
             <ModeSelector change = {this.modeSelectHandler}></ModeSelector>
-
+            <FileUpload uploadFile = {this.fileUploadHandler}/>
             <CodeOutput language={this.state.language} code = {this.state.code}></CodeOutput>
         </div>    
    
